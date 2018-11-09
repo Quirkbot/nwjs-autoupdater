5661075178ac8ae5319c27762a4667d4da6a5cd0
Paulo Barcelos <paulo@azucrina.org>
2018-11-09 15:40:28 +0100

    Custom build and added binaries

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm -rf dist
+mkdir -p dist/darwin/x64
+mkdir -p dist/win32/ia32
+mkdir -p dist/win32/x64
+cd src/nwjs-autoupdater/
+rsrc -manifest nwjs-autoupdater.exe.manifest -o nwjs-autoupdater.syso
+glide install
+cd ../../
+GOPATH=$(pwd) GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" nwjs-autoupdater
+mv nwjs-autoupdater dist/darwin/x64/autoupdater
+GOPATH=$(pwd) GOOS=windows GOARCH=386 go build -ldflags "-s -w -H=windowsgui" nwjs-autoupdater
+mv nwjs-autoupdater.exe dist/win32/ia32/autoupdater.exe
+GOPATH=$(pwd) GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -H=windowsgui" nwjs-autoupdater
+mv nwjs-autoupdater.exe dist/win32/x64/autoupdater.exe
