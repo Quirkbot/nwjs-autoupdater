62ecc497e53ed0498b57e4e2c62d118b0b6fcf7e
Murilo Polese <murilopolese@gmail.com>
2019-01-09 11:33:18 +0000

    Add linux build

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@ rm -rf dist
 mkdir -p dist/darwin/x64
 mkdir -p dist/win32/ia32
 mkdir -p dist/win32/x64
+mkdir -p dist/linux/x64
 cd src/nwjs-autoupdater/
 rsrc -manifest nwjs-autoupdater.exe.manifest -o nwjs-autoupdater.syso
 glide install
 cd ../../
+GOPATH=$(pwd) GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" nwjs-autoupdater
+mv nwjs-autoupdater dist/linux/x64/autoupdater
 GOPATH=$(pwd) GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" nwjs-autoupdater
 mv nwjs-autoupdater dist/darwin/x64/autoupdater
 GOPATH=$(pwd) GOOS=windows GOARCH=386 go build -ldflags "-s -w -H=windowsgui" nwjs-autoupdater
