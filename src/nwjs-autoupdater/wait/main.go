92c205e4ec2376335308a209fa54f26b10391363
Murilo Polese <murilopolese@gmail.com>
2019-01-10 16:17:36 +0000

    Adding timeout to `WaitProcess`

--- src/nwjs-autoupdater/wait/main.go
+++ src/nwjs-autoupdater/wait/main.go
@@ -8,11 +8,13 @@ import (
 
 func WaitProcess(processId int, logger *log.Logger) {
 	running := true
-	for running {
+	tries := 0
+	for running && tries < 60 {
 		running = isrunning.IsRunning(processId, logger)
 		logger.Printf("process %s still running: %d\n", processId, running)
 		if running {
 			time.Sleep(1000 * time.Millisecond)
+			tries += 1
 		}
 	}
 }
