dcea7f99da8a6878c3322bb0e123627ac0de1d84
Murilo Polese <murilopolese@gmail.com>
2019-01-15 11:29:40 +0000

    Hard tabs

--- src/nwjs-autoupdater/updater/updater_windows.go
+++ src/nwjs-autoupdater/updater/updater_windows.go
@@ -8,12 +8,12 @@ import (
 
 func Update(bundle, instDir, appName string) (error, string) {
 	appExecName := appName + ".exe"
-  appExec := filepath.Join(instDir, appExecName)
+	appExec := filepath.Join(instDir, appExecName)
 
-  err := unzip.Unzip(bundle, instDir)
+	err := unzip.Unzip(bundle, instDir)
 	if err != nil {
 		return err, appExec
 	}
 
-  return nil, appExec
+	return nil, appExec
 }
